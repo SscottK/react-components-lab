f8c6715531d1f3ec9c395cf22db14b3626061cc4
SscottK <gr8whtninjassk@gmail.com>
2024-09-15 15:59:19 -0700

    app shows components properly

---
+++ src/WeatherForcast.jsx
@@ -0,0 +1,14 @@
+import "./WeatherForecast.css"
+
+const WeatherForcast = ({ forecast }) => {
+    return (
+        <div className="weather">
+            <h2>{forecast.day}</h2>
+            <img src={forecast.img} alt={forecast.imgAlt} />
+            <p><span>conditions: </span>{forecast.conditions}</p>
+            <p><span>time: </span>{forecast.time}</p>
+        </div>          
+    )
+};
+
+export default WeatherForcast;
